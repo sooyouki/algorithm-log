906eb4465624da2574a227d46b165ce6b514f0d1
swim <139524522+sooyouki@users.noreply.github.com>
2026-02-24 17:43:41 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+p = [0] + list(map(int, sys.stdin.readline().split()))
+
+dp = [0] * (N + 1)
+
+for k in range(1, N + 1):
+    dp[k] = min([(dp[k - i] + p[i]) for i in range(1, k + 1)])
+
+print(dp[N])
