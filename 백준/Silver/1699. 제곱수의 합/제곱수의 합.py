9a6d216f8d3c522a6a65393fdc95513896adfb66
swim <139524522+sooyouki@users.noreply.github.com>
2026-03-02 18:37:56 +0900

    [Silver II] Title: 제곱수의 합, Time: 5580 ms, Memory: 35320 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,13 +1,16 @@
-import sys
+import sys, math
 
 N = int(sys.stdin.readline())
 dp = [0] * (N + 1)
 
 for i in range(1, N + 1):
     dp[i] = i
-    if (i ** (1 / 2)).is_integer():
+    limit = math.isqrt(i)
+
+    if i == limit**2:
         dp[i] = 1
-    for j in range(int(i ** (1 / 2)), 1, -1):
-        dp[i] = min(dp[i], dp[j**2] + dp[i - j**2])
+
+    for j in range(1, limit + 1):
+        dp[i] = min(dp[i], 1 + dp[i - j**2])
 
 print(dp[N])
